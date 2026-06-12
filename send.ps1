cd2be8f78a5dfe72779f40140661e1b984f0c1cc
jaroenchaisaikong2-droid <jaroenchaisaikong2@gmail.com>
2026-06-12 22:07:10 +0700

    Update message to support Thai language and UTF-8

--- send.ps1
+++ send.ps1
@@ -1,10 +1,14 @@
+# บังคับให้ PowerShell อ่านและเขียนเป็น UTF-8
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
 $token = $env:LINE_TOKEN
 $groupId = $env:LINE_GROUP_ID
-$msg = "Daily Report: Link is ready at https://www.google.com"
+# ตอนนี้คุณสามารถใส่ภาษาไทยได้แล้ว
+$msg = "รายงานประจำวัน: ลิงก์งานวันนี้พร้อมแล้วครับ https://www.google.com"
 
 $body = @{
     to = $groupId
     messages = @(@{ type = "text"; text = $msg })
 } | ConvertTo-Json -Compress
 
-Invoke-RestMethod -Uri "https://api.line.me/v2/bot/message/push" -Method Post -Headers @{ "Authorization" = "Bearer $token"; "Content-Type" = "application/json" } -Body $body
+Invoke-RestMethod -Uri "https://api.line.me/v2/bot/message/push" -Method Post -Headers @{ "Authorization" = "Bearer $token"; "Content-Type" = "application/json; charset=utf-8" } -Body $body
