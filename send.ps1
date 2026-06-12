89cd9a143e165328a1efdefc82e2f8f52d7fcb3d
jaroenchaisaikong2-droid <jaroenchaisaikong2@gmail.com>
2026-06-12 22:40:04 +0700

    Update send.ps1

--- send.ps1
+++ send.ps1
@@ -1,12 +1,15 @@
 $token = $env:LINE_TOKEN
 $groupId = $env:LINE_GROUP_ID
 
-# อ่านไฟล์ และใช้ .Trim() เพื่อลบช่องว่างหรือบรรทัดใหม่ที่เกินมาที่ต้นและท้ายข้อความออก
-$msg = (Get-Content -Path "./message.txt" -Encoding UTF8).Trim()
+# อ่านข้อความจากไฟล์ message.txt ตรงๆ เลย
+$msg = (Get-Content -Path "./message.txt").Trim()
 
 $body = @{
     to = $groupId
     messages = @(@{ type = "text"; text = $msg })
 } | ConvertTo-Json -Compress
 
-Invoke-RestMethod -Uri "https://api.line.me/v2/bot/message/push" -Method Post -Headers @{ "Authorization" = "Bearer $token"; "Content-Type" = "application/json; charset=utf-8" } -Body $body
+Invoke-RestMethod -Uri "https://api.line.me/v2/bot/message/push" `
+                  -Method Post `
+                  -Headers @{ "Authorization" = "Bearer $token"; "Content-Type" = "application/json; charset=utf-8" } `
+                  -Body $body
