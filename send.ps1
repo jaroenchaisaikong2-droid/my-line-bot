a7dcb169f8ffbd59e236da1306351acfe2eb0049
jaroenchaisaikong2-droid <jaroenchaisaikong2@gmail.com>
2026-06-12 22:04:31 +0700

    Update LINE message content and group ID variable

--- send.ps1
+++ send.ps1
@@ -1,7 +1,10 @@
 $token = $env:LINE_TOKEN
-$groupId = $env:LINE_GROUP_ID_TEST
+$groupId = $env:LINE_GROUP_ID
+$msg = "Daily Report: Link is ready at https://www.google.com"
+
 $body = @{
     to = $groupId
-    messages = @(@{ type = "text"; text = "สวัสดีครับ! นี่คือลิงก์งานวันนี้ https://www.google.com" })
-} | ConvertTo-Json
+    messages = @(@{ type = "text"; text = $msg })
+} | ConvertTo-Json -Compress
+
 Invoke-RestMethod -Uri "https://api.line.me/v2/bot/message/push" -Method Post -Headers @{ "Authorization" = "Bearer $token"; "Content-Type" = "application/json" } -Body $body
